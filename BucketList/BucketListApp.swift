2cc2b543cddf2d379f6c301d1555f03cab3f53c8
Ramsay Leung <ramsayleung@gmail.com>
2024-03-08 17:48:01 -0800

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by ramsayleung on 2024-03-08.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
